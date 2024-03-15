e20c80742f23f7adcb40fe8a4718726c590cd551
yvillegas <villegasyanina.c@gmail.com>
2024-03-15 18:34:20 -0500

    create request builder class

---
+++ lib/core/request_builder.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:footloose_app/core/error/exceptions.dart';
+import 'package:http/http.dart';
+
+enum RequestType { get, post }
+
+class RequestBuilder {
+  RequestBuilder({
+    required this.httpClient,
+  });
+
+  final Client httpClient;
+
+  Future<Response> request(
+    String endpoint,
+    RequestType requestType, {
+    Map<String, dynamic>? body,
+  }) async {
+    const apiHost = '192.168.0.12:3000';
+    final url = Uri.http(apiHost, endpoint);
+    try {
+      final headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      };
+      switch (requestType) {
+        case RequestType.get:
+          return await httpClient.get(url, headers: headers);
+        case RequestType.post:
+          print(body);
+          return await httpClient.post(
+            url,
+            headers: headers,
+            body: json.encode(body),
+          );
+      }
+    } catch (e) {
+      throw RequestException();
+    }
+  }
+}
