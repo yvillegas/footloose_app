0b9b3362ce47785c45320742c1fa54fcb8e1aa07
yvillegas <villegasyanina.c@gmail.com>
2024-03-16 11:35:09 -0500

    update request builder for add authentication

--- lib/core/request_builder.dart
+++ lib/core/request_builder.dart
@@ -1,21 +1,26 @@
 import 'dart:convert';
 
 import 'package:footloose_app/core/error/exceptions.dart';
+import 'package:footloose_app/core/shared_preferences_key.dart';
 import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum RequestType { get, post }
 
 class RequestBuilder {
   RequestBuilder({
     required this.httpClient,
+    required this.sharedPreferences,
   });
 
   final Client httpClient;
+  final SharedPreferences sharedPreferences;
 
   Future<Response> request(
     String endpoint,
     RequestType requestType, {
     Map<String, dynamic>? body,
+    bool withAuthentication = false,
   }) async {
     const apiHost = '192.168.0.12:3000';
     final url = Uri.http(apiHost, endpoint);
@@ -24,6 +29,10 @@ class RequestBuilder {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
       };
+      if (withAuthentication) {
+        final token = sharedPreferences.getString(Preferences.token) ?? '';
+        headers.addAll({'x-token': token});
+      }
       switch (requestType) {
         case RequestType.get:
           return await httpClient.get(url, headers: headers);
