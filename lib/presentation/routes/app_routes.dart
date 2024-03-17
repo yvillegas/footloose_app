652970e958d01ff268dba33477958494a0a3d124
yvillegas <villegasyanina.c@gmail.com>
2024-03-17 12:11:31 -0500

    add route for product detail page

--- lib/presentation/routes/app_routes.dart
+++ lib/presentation/routes/app_routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:footloose_app/presentation/ui/auth/login/login_page.dart';
+import 'package:footloose_app/presentation/ui/products/product_detail/product_detail_page.dart';
 import 'package:footloose_app/presentation/ui/products/product_list/product_list_page.dart';
 
 class AppRoutes {
   static const login = '/login';
   static const productList = '/productList';
+  static const productDetail = '/productDetail';
 }
 
 Route<dynamic>? generateRoutes(RouteSettings settings) {
@@ -16,6 +18,13 @@ Route<dynamic>? generateRoutes(RouteSettings settings) {
     case AppRoutes.productList:
       return MaterialPageRoute<void>(
         builder: (_) => const ProductListPage(),
+      );
+    case AppRoutes.productDetail:
+      final arguments = settings.arguments as ProductDetailPageArguments?;
+      return MaterialPageRoute<void>(
+        builder: (_) => ProductDetailPage(
+          id: arguments!.productoId,
+        ),
       );
     default:
       return null;
